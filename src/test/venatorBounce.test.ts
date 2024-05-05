5cfb8c38bce0618a6b000aa4c9c662bba2d763a7
supalosa <supalosa@supalosa.com>
2024-05-05 13:57:10 +1000

    fix bounce logic and add tests for sources up to 4x4

--- src/test/venatorBounce.test.ts
+++ src/test/venatorBounce.test.ts
@@ -3,15 +3,419 @@ import { canBounce } from "../venator";
 
 // https://docs.google.com/spreadsheets/d/e/2PACX-1vSN8AyqTbbBAuazJBoqlCw3EuG7_nuW4YVyQDtl6cNlisc1OQj7RAE7qZSrR9YPOl_fYv3-WnGoRIFb/pubhtml
 describe("venator bounce tests 1x1 source", () => {
-  test("cannot bounce to 1x1 3 tiles away", () => {
-    expect(canBounce(0, 0, 1, 3, 0, 1)).toBe(false);
-  });
-  
-  test("can bounce to all 1x1 2 tiles away", () => {
-    for (let x = -2; x <= 2; ++x) {
-      for (let y = -2; y <= 2; ++y) {
-        expect(canBounce(0, 0, 1, x, y, 1)).toBe(true);
+  const SIZE = 1;
+
+  describe("to 1x1 target", () => {
+    test("cannot bounce to 1x1 more than 2 tiles away", () => {
+      for (let x = -5; x <= 5; ++x) {
+        for (let y = -5; y <= 5; ++y) {
+          if (x >= -2 && x <= 2 && y >= -2 && y <= 2) {
+            continue;
+          }
+          expect(canBounce(0, 0, SIZE, x, y, 1), `${x}, ${y}`).toBe(false);
+        }
       }
-    }
+    });
+    test("can bounce to all 1x1 2 tiles away", () => {
+      for (let x = -2; x <= 2; ++x) {
+        for (let y = -2; y <= 2; ++y) {
+          expect(canBounce(0, 0, SIZE, x, y, 1), `${x}, ${y}`).toBe(true);
+        }
+      }
+    });
+  });
+
+  describe("to 2x2 target", () => {
+    test("cannot bounce to 2x2 more than 2 tiles away", () => {
+      for (let x = -5; x <= 5; ++x) {
+        for (let y = -5; y <= 5; ++y) {
+          if (x >= -2 && x <= 2 && y >= -2 && y <= 2) {
+            continue;
+          }
+          expect(canBounce(0, 0, SIZE, x, y, 2), `${x}, ${y}`).toBe(false);
+        }
+      }
+    });
+    test("can bounce to all 2x2 2 tiles away", () => {
+      for (let x = -2; x <= 2; ++x) {
+        for (let y = -2; y <= 2; ++y) {
+          expect(canBounce(0, 0, SIZE, x, y, 2), `${x}, ${y}`).toBe(true);
+        }
+      }
+    });
+  });
+
+  describe("to 3x3 target", () => {
+    test("cannot bounce to 3x3 outside of the 4x4 square", () => {
+      for (let x = -5; x <= 5; ++x) {
+        for (let y = -5; y <= 5; ++y) {
+          if (x >= -2 && x <= 1 && y >= -1 && y <= 2) {
+            continue;
+          }
+          expect(canBounce(0, 0, SIZE, x, y, 3), `${x}, ${y}`).toBe(false);
+        }
+      }
+    });
+    test("can bounce to all 3x3 within a 4x4 square", () => {
+      for (let x = -2; x <= 1; ++x) {
+        for (let y = -1; y <= 2; ++y) {
+          expect(canBounce(0, 0, SIZE, x, y, 3), `${x}, ${y}`).toBe(true);
+        }
+      }
+    });
+  });
+
+  describe("to 4x4 target", () => {
+    test("cannot bounce to 4x4 outside of the 3x3 square", () => {
+      for (let x = -5; x <= 5; ++x) {
+        for (let y = -5; y <= 5; ++y) {
+          if (x >= -2 && x <= 0 && y >= 0 && y <= 2) {
+            continue;
+          }
+          expect(canBounce(0, 0, SIZE, x, y, 4), `${x}, ${y}`).toBe(false);
+        }
+      }
+    });
+    test("can bounce to all 4x4 within a 3x3 square", () => {
+      for (let x = -2; x <= 0; ++x) {
+        for (let y = 0; y <= 2; ++y) {
+          expect(canBounce(0, 0, SIZE, x, y, 4), `${x}, ${y}`).toBe(true);
+        }
+      }
+    });
+  });
+
+  describe("to 5x5 target", () => {
+    test("cannot bounce to 5x5 outside of the 3x3 square", () => {
+      for (let x = -5; x <= 5; ++x) {
+        for (let y = -5; y <= 5; ++y) {
+          if (x >= -2 && x <= 0 && y >= 0 && y <= 2) {
+            continue;
+          }
+          expect(canBounce(0, 0, SIZE, x, y, 5), `${x}, ${y}`).toBe(false);
+        }
+      }
+    });
+    test("can bounce to all 5x5 within a 3x3 square", () => {
+      for (let x = -2; x <= 0; ++x) {
+        for (let y = 0; y <= 2; ++y) {
+          expect(canBounce(0, 0, SIZE, x, y, 5), `${x}, ${y}`).toBe(true);
+        }
+      }
+    });
   });
 });
+
+describe("venator bounce tests 2x2 source", () => {
+  const SIZE = 2;
+
+  describe("to 1x1 target", () => {
+    test("cannot bounce to 1x1 more than 3 tiles away", () => {
+      for (let x = -5; x <= 5; ++x) {
+        for (let y = -5; y <= 5; ++y) {
+          if (x >= -2 && x <= 3 && y >= -3 && y <= 2) {
+            continue;
+          }
+          expect(canBounce(0, 0, SIZE, x, y, 1), `${x}, ${y}`).toBe(false);
+        }
+      }
+    });
+    test("can bounce to all 1x1 3 tiles away", () => {
+      for (let x = -2; x <= 3; ++x) {
+        for (let y = -3; y <= 2; ++y) {
+          expect(canBounce(0, 0, SIZE, x, y, 1), `${x}, ${y}`).toBe(true);
+        }
+      }
+    });
+  });
+
+  describe("to 2x2 target", () => {
+    test("cannot bounce to 2x2 more than 3 tiles away", () => {
+      for (let x = -5; x <= 5; ++x) {
+        for (let y = -5; y <= 5; ++y) {
+          if (x >= -2 && x <= 3 && y >= -3 && y <= 2) {
+            continue;
+          }
+          expect(canBounce(0, 0, SIZE, x, y, 2), `${x}, ${y}`).toBe(false);
+        }
+      }
+    });
+    test("can bounce to all 2x2 3 tiles away", () => {
+      for (let x = -2; x <= 3; ++x) {
+        for (let y = -3; y <= 2; ++y) {
+          expect(canBounce(0, 0, SIZE, x, y, 2), `${x}, ${y}`).toBe(true);
+        }
+      }
+    });
+  });
+
+  describe("to 3x3 target", () => {
+    test("cannot bounce to 3x3 outside of the 6x6 square", () => {
+      for (let x = -5; x <= 5; ++x) {
+        for (let y = -5; y <= 5; ++y) {
+          if (x >= -3 && x <= 2 && y >= -2 && y <= 3) {
+            continue;
+          }
+          expect(canBounce(0, 0, SIZE, x, y, 3), `${x}, ${y}`).toBe(false);
+        }
+      }
+    });
+    test("can bounce to all 3x3 within a 6x6 square", () => {
+      for (let x = -3; x <= 2; ++x) {
+        for (let y = -2; y <= 3; ++y) {
+          expect(canBounce(0, 0, SIZE, x, y, 3), `${x}, ${y}`).toBe(true);
+        }
+      }
+    });
+  });
+
+  describe("to 4x4 target", () => {
+    test("cannot bounce to 4x4 outside of the 5x5 square", () => {
+      for (let x = -5; x <= 5; ++x) {
+        for (let y = -5; y <= 5; ++y) {
+          if (x >= -3 && x <= 1 && y >= -1 && y <= 3) {
+            continue;
+          }
+          expect(canBounce(0, 0, SIZE, x, y, 4), `${x}, ${y}`).toBe(false);
+        }
+      }
+    });
+    test("can bounce to all 4x4 within a 5x5 square", () => {
+      for (let x = -3; x <= 1; ++x) {
+        for (let y = -1; y <= 3; ++y) {
+          expect(canBounce(0, 0, SIZE, x, y, 4), `${x}, ${y}`).toBe(true);
+        }
+      }
+    });
+  });
+
+  describe("to 5x5 target", () => {
+    test("cannot bounce to 5x5 outside of the 5x5 square", () => {
+      for (let x = -5; x <= 5; ++x) {
+        for (let y = -5; y <= 5; ++y) {
+          if (x >= -3 && x <= 1 && y >= -1 && y <= 3) {
+            continue;
+          }
+          expect(canBounce(0, 0, SIZE, x, y, 5), `${x}, ${y}`).toBe(false);
+        }
+      }
+    });
+    test("can bounce to all 5x5 within a 5x5 square", () => {
+      for (let x = -3; x <= 1; ++x) {
+        for (let y = -1; y <= 3; ++y) {
+          expect(canBounce(0, 0, SIZE, x, y, 5), `${x}, ${y}`).toBe(true);
+        }
+      }
+    });
+  });
+});
+
+describe("venator bounce tests 3x3 source", () => {
+  const SIZE = 3;
+
+  describe("to 1x1 target", () => {
+    test("cannot bounce to 1x1 more than 2 tiles away", () => {
+      for (let x = -5; x <= 5; ++x) {
+        for (let y = -5; y <= 5; ++y) {
+          if (x >= -1 && x <= 3 && y >= -3 && y <= 1) {
+            continue;
+          }
+          expect(canBounce(0, 0, SIZE, x, y, 1), `${x}, ${y}`).toBe(false);
+        }
+      }
+    });
+    test("can bounce to all 1x1 3 tiles away", () => {
+      for (let x = -1; x <= 3; ++x) {
+        for (let y = -3; y <= 1; ++y) {
+          expect(canBounce(0, 0, SIZE, x, y, 1), `${x}, ${y}`).toBe(true);
+        }
+      }
+    });
+  });
+
+  describe("to 2x2 target", () => {
+    test("cannot bounce to 2x2 more than 3 tiles away", () => {
+      for (let x = -5; x <= 5; ++x) {
+        for (let y = -5; y <= 5; ++y) {
+          if (x >= -1 && x <= 3 && y >= -3 && y <= 1) {
+            continue;
+          }
+          expect(canBounce(0, 0, SIZE, x, y, 2), `${x}, ${y}`).toBe(false);
+        }
+      }
+    });
+    test("can bounce to all 2x2 3 tiles away", () => {
+      for (let x = -1; x <= 3; ++x) {
+        for (let y = -3; y <= 1; ++y) {
+          expect(canBounce(0, 0, SIZE, x, y, 2), `${x}, ${y}`).toBe(true);
+        }
+      }
+    });
+  });
+
+  describe("to 3x3 target", () => {
+    test("cannot bounce to 3x3 outside of the 4x4 square", () => {
+      for (let x = -5; x <= 5; ++x) {
+        for (let y = -5; y <= 5; ++y) {
+          if (x >= -1 && x <= 2 && y >= -2 && y <= 1) {
+            continue;
+          }
+          expect(canBounce(0, 0, SIZE, x, y, 3), `${x}, ${y}`).toBe(false);
+        }
+      }
+    });
+    test("can bounce to all 3x3 within a 4x4 square", () => {
+      for (let x = -1; x <= 2; ++x) {
+        for (let y = -2; y <= 1; ++y) {
+          expect(canBounce(0, 0, SIZE, x, y, 3), `${x}, ${y}`).toBe(true);
+        }
+      }
+    });
+  });
+
+  describe("to 4x4 target", () => {
+    test("cannot bounce to 4x4 outside of the 3x3 square", () => {
+      for (let x = -5; x <= 5; ++x) {
+        for (let y = -5; y <= 5; ++y) {
+          if (x >= -1 && x <= 1 && y >= -1 && y <= 1) {
+            continue;
+          }
+          expect(canBounce(0, 0, SIZE, x, y, 4), `${x}, ${y}`).toBe(false);
+        }
+      }
+    });
+    test("can bounce to all 4x4 within a 3x3 square", () => {
+      for (let x = -1; x <= 1; ++x) {
+        for (let y = -1; y <= 1; ++y) {
+          expect(canBounce(0, 0, SIZE, x, y, 4), `${x}, ${y}`).toBe(true);
+        }
+      }
+    });
+  });
+
+  describe("to 5x5 target", () => {
+    test("cannot bounce to 5x5 outside of the 3x3 square", () => {
+      for (let x = -5; x <= 5; ++x) {
+        for (let y = -5; y <= 5; ++y) {
+          if (x >= -1 && x <= 1 && y >= -1 && y <= 1) {
+            continue;
+          }
+          expect(canBounce(0, 0, SIZE, x, y, 5), `${x}, ${y}`).toBe(false);
+        }
+      }
+    });
+    test("can bounce to all 5x5 within a 3x3 square", () => {
+      for (let x = -1; x <= 1; ++x) {
+        for (let y = -1; y <= 1; ++y) {
+          expect(canBounce(0, 0, SIZE, x, y, 5), `${x}, ${y}`).toBe(true);
+        }
+      }
+    });
+  });
+});
+
+describe("venator bounce tests 4x4 source", () => {
+  const SIZE = 4;
+
+  describe("to 1x1 target", () => {
+    test("cannot bounce to 1x1 more outside of 6x6 square", () => {
+      for (let x = -5; x <= 5; ++x) {
+        for (let y = -5; y <= 5; ++y) {
+          if (x >= -1 && x <= 4 && y >= -4 && y <= 1) {
+            continue;
+          }
+          expect(canBounce(0, 0, SIZE, x, y, 1), `${x}, ${y}`).toBe(false);
+        }
+      }
+    });
+    test("can bounce to all 1x1 within a 6x6 square", () => {
+      for (let x = -1; x <= 4; ++x) {
+        for (let y = -4; y <= 1; ++y) {
+          expect(canBounce(0, 0, SIZE, x, y, 1), `${x}, ${y}`).toBe(true);
+        }
+      }
+    });
+  });
+
+  describe("to 2x2 target", () => {
+    test("cannot bounce to 2x2 more outside of 6x6 square", () => {
+      for (let x = -5; x <= 5; ++x) {
+        for (let y = -5; y <= 5; ++y) {
+          if (x >= -1 && x <= 4 && y >= -4 && y <= 1) {
+            continue;
+          }
+          expect(canBounce(0, 0, SIZE, x, y, 2), `${x}, ${y}`).toBe(false);
+        }
+      }
+    });
+    test("can bounce to all 2x2 within 6x6 square", () => {
+      for (let x = -1; x <= 4; ++x) {
+        for (let y = -4; y <= 1; ++y) {
+          expect(canBounce(0, 0, SIZE, x, y, 2), `${x}, ${y}`).toBe(true);
+        }
+      }
+    });
+  });
+
+  describe("to 3x3 target", () => {
+    test("cannot bounce to 3x3 outside of the 6x6 square", () => {
+      for (let x = -5; x <= 5; ++x) {
+        for (let y = -5; y <= 5; ++y) {
+          if (x >= -1 && x <= 4 && y >= -4 && y <= 1) {
+            continue;
+          }
+          expect(canBounce(0, 0, SIZE, x, y, 3), `${x}, ${y}`).toBe(false);
+        }
+      }
+    });
+    test("can bounce to all 3x3 within a 6x6 square", () => {
+      for (let x = -1; x <= 4; ++x) {
+        for (let y = -4; y <= 1; ++y) {
+          expect(canBounce(0, 0, SIZE, x, y, 3), `${x}, ${y}`).toBe(true);
+        }
+      }
+    });
+  });
+
+  describe("to 4x4 target", () => {
+    test("cannot bounce to 4x4 outside of the 5x5 square", () => {
+      for (let x = -5; x <= 5; ++x) {
+        for (let y = -5; y <= 5; ++y) {
+          if (x >= -1 && x <= 3 && y >= -3 && y <= 1) {
+            continue;
+          }
+          expect(canBounce(0, 0, SIZE, x, y, 4), `${x}, ${y}`).toBe(false);
+        }
+      }
+    });
+    test("can bounce to all 4x4 within a 5x5 square", () => {
+      for (let x = -1; x <= 3; ++x) {
+        for (let y = -3; y <= 1; ++y) {
+          expect(canBounce(0, 0, SIZE, x, y, 4), `${x}, ${y}`).toBe(true);
+        }
+      }
+    });
+  });
+
+  describe("to 5x5 target", () => {
+    test("cannot bounce to 5x5 outside of the 5x5 square", () => {
+      for (let x = -5; x <= 5; ++x) {
+        for (let y = -5; y <= 5; ++y) {
+          if (x >= -1 && x <= 3 && y >= -3 && y <= 1) {
+            continue;
+          }
+          expect(canBounce(0, 0, SIZE, x, y, 5), `${x}, ${y}`).toBe(false);
+        }
+      }
+    });
+    test("can bounce to all 5x5 within a 5x5 square", () => {
+      for (let x = -1; x <= 3; ++x) {
+        for (let y = -3; y <= 1; ++y) {
+          expect(canBounce(0, 0, SIZE, x, y, 5), `${x}, ${y}`).toBe(true);
+        }
+      }
+    });
+  });
+});
+
+// should add 5x5 source but we don't need it
--- src/venator.ts
+++ src/venator.ts
@@ -6,9 +6,10 @@ const isInRange = ([x, y]: [number, number], [x2, y2]: [number, number]) => {
   return dxAbs <= RANGE && dyAbs <= RANGE;
 };
 
-// *all* of the coordinates in `to` have to be in range
+// only one of the coordinates in `from` have to pass
+// *all* of the coordinates in `to` have to be in range of a coordinate in `from`
 const anyInRange = (from: [number, number][], to: [number, number][]) => {
-    return from.some(f => to.every(t => isInRange(f, t)));
+  return from.some((f) => to.every((t) => isInRange(f, t)));
 };
 
 // https://docs.google.com/document/d/e/2PACX-1vR5Sc78Wg6UDmKZ4QTtkuG9NtcM-GjFEaofZ77Z0MhM9NF5CeqLNCTx6E7l3JA6pBQxIsw--vRX8Y7M/pub
@@ -20,28 +21,28 @@ export function canBounce(
   y2: number,
   size2: number
 ): boolean {
-  const [sw, centerSw, center] = getScanTiles(x, y, size);
+  const [, , center] = getScanTiles(x, y, size);
   const [sw2, centerSw2, center2] = getScanTiles(x2, y2, size2);
   switch (size) {
     case 1:
     case 3:
     case 5:
-        // odd size sends bounce if it finds targets SW and CENTER tiles from its CENTER tile
-        return anyInRange([center], [sw2, center2]);
+      // odd size sends bounce if it finds targets SW and CENTER tiles from its CENTER tile
+      return anyInRange([center], [sw2, center2]);
     case 2:
-        const all2x2 = getAllTiles(x, y, size);
-        if (size2 <= 3) {
-            // If it finds targets CENTER tile from any of its tiles (monster is ≤ 3x3).
-            return anyInRange(all2x2, [center2]);
-        } else if (size2 <= 5) {
-            //If it finds targets CENTER and CENTER SW tile from any of its tiles (monster is ≥ 4x4).
-            return anyInRange(all2x2, [centerSw2, center2]);
-        }
-        break;
+      const all2x2 = getAllTiles(x, y, size);
+      if (size2 <= 3) {
+        // If it finds targets CENTER tile from any of its tiles (monster is ≤ 3x3).
+        return anyInRange(all2x2, [center2]);
+      } else if (size2 <= 5) {
+        //If it finds targets CENTER and CENTER SW tile from any of its tiles (monster is ≥ 4x4).
+        return anyInRange(all2x2, [centerSw2, center2]);
+      }
+      break;
     case 4:
-        // 4x4 sends bounce if it finds targets SW and CENTER SW tiles from any of its middle 2x2 tiles.
-        const middle2x2 = getAllTiles(x + 1, y - 1, size);
-        return anyInRange(middle2x2, [sw2, centerSw2]);
+      // 4x4 sends bounce if it finds targets SW and CENTER SW tiles from any of its middle 2x2 tiles.
+      const middle2x2 = getAllTiles(x + 1, y - 1, 2);
+      return anyInRange(middle2x2, [sw2, centerSw2]);
   }
   throw new Error(`Unsupported bounce check (sizes ${size} vs ${size2})`);
 }
@@ -67,15 +68,15 @@ function getScanTiles(x: number, y: number, size: number): [Tile, Tile, Tile] {
     case 3:
       return [
         [x, y],
-        [x + 1, y - 1],
         [x, y],
+        [x + 1, y - 1],
       ];
     case 4:
     case 5:
       return [
         [x, y],
         [x + 1, y - 1],
-        [x - 2, y - 2],
+        [x + 2, y - 2],
       ];
     default:
       throw new Error(`Unsupported NPC size ${size}`);
