71b936c11c18b9703e7f91fb107b83f568e78283
Supalosa <supalosa@supalosa.com>
2024-11-02 19:10:48 +1100

    Install react

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
+import react from '@vitejs/plugin-react'
+
 const BASE_URL = process.env.BASE_URL || '';
 
 export default {
-  base: `/${BASE_URL}`
+  base: `/${BASE_URL}`,
+  plugins: [react()]
 }
