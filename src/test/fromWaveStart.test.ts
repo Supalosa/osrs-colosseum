81bbcf83847bb4d13bfd0b682870c1dfed6546e5
Supalosa <Supalosa>
2025-07-08 00:08:51 +1000

    test not attacking

--- src/test/fromWaveStart.test.ts
+++ src/test/fromWaveStart.test.ts
@@ -1,39 +1,76 @@
 import { afterAll, beforeAll, describe, expect, test } from "vitest";
-import { _getMobs, _setSelected, place, reset, setFromWaveStart, step } from "../lineOfSight";
+import { _getMobs, _setSelected, place, remove, setFromWaveStart, step } from "../lineOfSight";
 import { Mob } from "../types";
-import { checkMove } from "./utils";
+import { checkIdleStep, checkMove } from "./utils";
 
 describe("fromWaveStart tests", () => {
   beforeAll(() => {
-    // this is a sequential test, so only reset state at the start
-    reset();
     setFromWaveStart(true);
   });
 
   afterAll(() => {
     setFromWaveStart(false);
-  })
-
-  test("empty state", () => {
-    expect(_getMobs()).toEqual([]);
   });
+
+  describe("movement checks", () => {
+    beforeAll(() => {
+      remove();
+    });
+
+    const javelin: Mob = [20, 13, 2, 20, 13, 0, null];
+
+    test("place single javelin", () => {
+      _setSelected([javelin[0], javelin[1]], javelin[2]);
+      place();
+      expect(_getMobs()).toEqual([javelin]);
+    });
+
+    test("check npc doesn't move on first tick", () => {
+      _setSelected([7, 8], 0);
+      step();
+      checkMove(javelin, 20, 13);
+    });
+
+    test("check npc moves on second tick", () => {
+      step();
+      checkMove(javelin, 19, 12);
+    });
+  });
+
   
-  const javelin: Mob = [20, 13, 2, 20, 13, 0, null] satisfies Mob;
+  describe("attack delay checks", () => {
+    beforeAll(() => {
+      remove();
+    });
 
-  test("place single javelin", () => {
-    _setSelected([javelin[0], javelin[1]], javelin[2]);
-    place();
-    expect(_getMobs()).toEqual([javelin]);
-  });
+    const javelin: Mob = [12, 12, 2, 12, 12, 0, null];
 
-  test("check npc doesn't move on first tick", () => {
-    _setSelected([7, 8], 0);
-    step();
-    checkMove(javelin, 20, 13);
-  });
+    test("place single javelin", () => {
+      _setSelected([javelin[0], javelin[1]], javelin[2]);
+      place();
+      _setSelected([16, 16], 0);
+      expect(_getMobs()).toEqual([javelin]);
+    });
 
-  test("check npc moves on second tick", () => {
-    step();
-    checkMove(javelin, 19, 12);
+    test("check npc doesn't attack on first tick", () => {
+      step();
+      checkIdleStep(javelin);
+    });
+
+    test("check npc doesn't attack on second tick", () => {
+      step();
+      checkIdleStep(javelin);
+    });
+
+    test("check npc doesn't attack on third tick", () => {
+      step();
+      checkIdleStep(javelin);
+    });
+
+    test("check npc fires on the fourth tick", () => {
+      step();
+      const mobs = _getMobs();
+      expect(mobs[0][5]).toEqual(5);
+    });
   });
 });
